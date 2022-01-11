411f6c13a416939f76f365188cced81fbde5af91
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-01-11 23:13:02 +0800

    card added

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,20 +1,24 @@
-import classes from './MeetupItem.module.css';
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
-function MeetupItem(props){
-    return <li className={classes.item}>
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <Card>
         <div className={classes.image}>
-            <img src={props.image} alt={props.title}/>
+          <img src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
-           <h3>{props.title}</h3>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-            <button>To Favorite</button>
+          <button>To Favorite</button>
         </div>
-        
+      </Card>
     </li>
+  );
 }
 
 export default MeetupItem;
