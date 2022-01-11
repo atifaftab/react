9570cae772325b786d5cfd9df03761a5139c13cb
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-01-11 22:22:07 +0800

    file added and some file removed all meetup and meetup list is up

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: 'm1',
@@ -21,11 +23,8 @@ const DUMMY_DATA = [
 function AllMeetupsPage() {
   return <section>
     <h1>All MeetUps</h1>
-    <ul>
-    {DUMMY_DATA.map( meetup => {
-      return <li key={meetup.id}>{meetup.title}</li>
-    })}
-    </ul>
+      <MeetupList meetups={DUMMY_DATA}/>
+
   </section>
 }
 export default AllMeetupsPage;
